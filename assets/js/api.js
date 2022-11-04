7f9a4cba639bd3f9d90e844836df2ec0d3b67492
Thamyres Magalhães <pink.thamyres@gmail.com>
2022-11-04 12:22:59 -0300

    alterando api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/Thamyresarm/dio-formacao-JavaScript/main/DesafiosdeProjetos/Portifolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Thamyresarm/Thamyresarm.github.io/main/data/profile.json';
     const fetching = await fetch(url);
     return fetching.json();
 }
